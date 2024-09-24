42f4ff0c887a4896f67dda08a224f8b87459c0e5
Yogesh-yo01 <syokesh081@gmail.com>
2024-09-24 13:32:45 +0530

    Error updates

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app=express();
 
 const dotenv=require('dotenv');
 const path = require('path');
+
 const cors = require('cors');
 
 
@@ -10,17 +11,19 @@ const connectDatabase = require('./config/connectDatabase');
 
 dotenv.config({path: path.join(__dirname,'config','config.env')})
 
-const corsOperation={
-    origin:process.env.APPLICATION_URL,
-    methods:'GET,HEAD,PUT,PATCH,POST,DELETE'
-};
+// const corsOperation={
+//     origin:process.env.APPLICATION_URL,
+//     methods:'GET,HEAD,PUT,PATCH,POST,DELETE'
+// };
 const Products =require('./routes/products');
 const orders =require('./routes/order');
 
 connectDatabase();
 
 app.use(express.json())
-app.use(cors(corsOperation));
+
+app.use(cors());
+
 app.use('/api/v1/',Products);
 app.use('/api/v1/',orders);
 
